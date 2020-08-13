288007f31b6028c7eb2ba86ff5ada1b0f42a04b3
yang <903435532@qq.com>
2020-08-13 16:46:42 +0800

    add

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from robotpi_movement import Movement
+from robotpi_serOp import serOp
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Covid():
+    def __init__(self):
+        self.ser = serOp()
+        self.movement = Movement()
+
+    def test(self):
+        self.movement.wave_hands()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    covid = Covid()
+    covid.test()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
