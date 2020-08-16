28dc103f48673922cdcee055a21df946a77c01aa
yang <903435532@qq.com>
2020-08-16 21:44:27 +0800

    add

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ class Covid():
         self.mv.set_volume(80)
         self.mv.play_sounds('0255\\0113.WAV')#请伸出手配合体温测量
         self.sensor.flushInput()#清除输入缓冲区数据
-        time.sleep(3)
+        time.sleep(3.5)
         data = self.sensor.readline()
         temperature = (data[0]-48) * 10 + (data[1]-48) + (data[3]-48) * 0.1
         while temperature < 30 or temperature > 40:
             self.mv.play_sounds('0255\\0113.WAV')  # 请伸出手配合体温测量
             self.sensor.flushInput()  # 清除输入缓冲区数据
-            time.sleep(3)
+            time.sleep(3.5)
             data = self.sensor.readline()
             temperature = (data[0] - 48) * 10 + (data[1] - 48) + (data[3] - 48) * 0.1
         self.mv.play_sounds('0255\\0112.WAV')  # 您的体温是
         time.sleep(2)
         self.mv.play_sounds('0255\\00'+str(data[0]-48)+str(data[1]-48)+'.WAV')
-        time.sleep(2)
+        time.sleep(1.5)
         self.mv.play_sounds('0255\\000' + str(data[3] - 48) + '.WAV')
         time.sleep(1)
         print("Your temperature is : {}".format(temperature))
@@ -35,10 +35,9 @@ class Covid():
 
     def run(self):
         while True:
-            # self.mv.action_2()
+            self.mv.action_2()
             self.read_temperature()
-            # time.sleep(2)
-            # self.mv.action_1()
+            self.mv.action_1()
             time.sleep(10)
 
 
