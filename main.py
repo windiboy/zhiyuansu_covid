3d527567ba16d295bd25877f156f0542d900f899
yang <903435532@qq.com>
2020-08-16 20:23:34 +0800

    add

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from robotpi_movement import Movement
 from robotpi_serOp import serOp
-
+import serial
+import time
 
 class Covid():
     def __init__(self):
         self.ser = serOp()
-        self.movement = Movement()
+        self.mv = Movement()
+        self.sensor = serial.Serial("/dev/ttyUSB1", 9600, timeout=0.5)
 
-    def test(self):
-        print(self.movement.wave_hands())
+    def read_temperature(self):
+        self.mv.set_volume(80)
+        self.mv.play_sounds('0255\\0113.WAV')#请伸出手配合体温测量
+        data = self.sensor.readline()
+        temperature = (data[0]-48) * 10 + (data[1]-48) + (data[3]-48) * 0.1
+        self.mv.play_sounds('0255\\0112.WAV')  # 您的体温是
+        self.mv.play_sounds('0255\\00'+str(data[0]-48)+str(data[1]-48)+'.WAV')
+        print("Your temperature is : {}".format(temperature))
+        if temperature>37.3:
+            self.mv.play_sounds('0255\\0111.WAV')  # 您的体温过高
+
+    def run(self):
+        while True:
+            # self.mv.action_2()
+            self.read_temperature()
+            # time.sleep(2)
+            # self.mv.action_1()
+            time.sleep(10)
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     covid = Covid()
-    covid.test()
+    covid.run()
 
