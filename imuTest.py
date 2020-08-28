240bb938e00e09a8657a3fe6c29ee1d0a31e85fd
yang <903435532@qq.com>
2020-08-28 15:22:10 +0800

    add

---
+++ imuTest.py
@@ -0,0 +1,23 @@
+from robotpi_movement import Movement
+from robotpi_serOp import serOp
+from WitSensor import ImuSensor
+import serial
+import time
+
+class Adjustment():
+    def __init__(self):
+        self.ser = serOp()
+        self.mv = Movement()
+        self.sensor = ImuSensor()
+
+    def run(self):
+        while True:
+            datahex = self.sensor.ser.read(33)
+            self.sensor.DueData(datahex)
+            print("Angle Y = {.3f}".format(self.sensor.a[1]))
+
+
+if __name__ == '__main__':
+    my = Adjustment()
+    my.run()
+
